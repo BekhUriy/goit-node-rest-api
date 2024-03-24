f5b0a5880c4cb765a172ee1c5ab11a0cd72f90c6
Bekhuriy <131916270+Bekhuriy@users.noreply.github.com>
2024-03-24 16:27:58 +0200

    add

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,58 @@
 import contactsService from "../services/contactsServices.js";
+import { validationResult } from "express-validator";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = (req, res) => {
+  const contacts = contactsService.listContacts();
+  res.status(200).json(contacts);
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = (req, res) => {
+  const { id } = req.params;
+  const contact = contactsService.getContactById(id);
+  if (contact) {
+    res.status(200).json(contact);
+  } else {
+    res.status(404).json({ message: "Not found" });
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = (req, res) => {
+  const { id } = req.params;
+  const deletedContact = contactsService.removeContact(id);
+  if (deletedContact) {
+    res.status(200).json(deletedContact);
+  } else {
+    res.status(404).json({ message: "Not found" });
+  }
+};
 
-export const createContact = (req, res) => {};
+export const createContact = (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ message: errors.array()[0].msg });
+  }
 
-export const updateContact = (req, res) => {};
+  const { name, email, phone } = req.body;
+  const newContact = contactsService.addContact(name, email, phone);
+  res.status(201).json(newContact);
+};
+
+export const updateContact = (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ message: errors.array()[0].msg });
+  }
+
+  const { id } = req.params;
+  const { name, email, phone } = req.body;
+  const updatedContact = contactsService.updateContact(id, {
+    name,
+    email,
+    phone,
+  });
+  if (updatedContact) {
+    res.status(200).json(updatedContact);
+  } else {
+    res.status(404).json({ message: "Not found" });
+  }
+};
---
+++ services/contactsServices.js
@@ -0,0 +1,58 @@
+const fs = require("node:fs/promises");
+const path = require("node:path");
+const crypto = require("node:crypto");
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+async function readContacts() {
+  const data = await fs.readContacts(filePath, "utf-8");
+  return JSON.parse(data);
+}
+
+async function writeContacts() {
+  console.log(contacts, typeof contacts);
+  return fs.writeFile(contactsPath, JSON.stringify(contacts, undefined, 2));
+}
+
+async function listContacts() {
+  console.log(contacts, typeof contacts);
+  return fs.listContacts(contactsPath, contacts);
+}
+
+async function getContactById(contactId) {
+  const contacts = await readContacts();
+  const contact = contacts.find((contact) => contact.id === id);
+  return contact;
+}
+
+async function removeContact(contactId) {
+  const contacts = await readContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index !== -1) {
+    const remove = contacts.splice(index, 1)[0];
+    await writeContacts(contacts);
+    return remove;
+  } else {
+    return null;
+  }
+}
+
+async function addContact(name, email, phone) {
+  const contact = await readContacts();
+  const newContact = {
+    ...contact,
+    id: crypto.randomUUID(),
+    name,
+    email,
+    phone,
+  };
+
+  contacts.push(newContact);
+  await writeContacts(contacts);
+  return newContact;
+}
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
