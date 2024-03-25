015117022b40f0a0877f773c3d3db858febd2680
Bekhuriy <131916270+Bekhuriy@users.noreply.github.com>
2024-03-25 02:53:47 +0200

    fix

--- router/contactsRouter.js
+++ router/contactsRouter.js
@@ -1,22 +1,8 @@
 import express from "express";
-import {
-  getAllContacts,
-  getOneContact,
-  deleteContact,
-  createContact,
-  updateContact,
-} from "../controllers/contactsControllers.js";
+import { updateContactFavorite } from "../controllers/contactsControllers.js";
 
 const contactsRouter = express.Router();
 
-contactsRouter.get("/", getAllContacts);
-
-contactsRouter.get("/:id", getOneContact);
-
-contactsRouter.delete("/:id", deleteContact);
-
-contactsRouter.post("/", createContact);
-
-contactsRouter.put("/:id", updateContact);
+contactsRouter.patch("/:contactId/favorite", updateContactFavorite);
 
 export default contactsRouter;
